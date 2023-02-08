d00a47fc55f3aed7b98a3b66020e959b2630d9b3
Joseafs <jose.fernandes@seedz.ag>
2023-02-08 09:17:54 -0300

    Insert/Run Storybook

--- scripts/publish.js
+++ scripts/publish.js
@@ -2,7 +2,7 @@ const { exec, execSync } = require('child_process')
 
 execSync('node ./scripts/build')
 
-exec('npm publish', { cwd: './dist' }, (err, stout, stderr) => {
+exec('yarn publish', { cwd: './dist' }, (err, stout, stderr) => {
   if (err) {
     console.log(stderr)
   } else {
